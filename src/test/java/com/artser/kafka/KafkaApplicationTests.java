c17cd3ac170af545506f2463198ec826c6a6c5d5
artser97 <artser97@yandex.ru>
2023-02-01 19:37:32 +0200

    kafka app 2 commit

---
+++ src/test/java/com/artser/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artser.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
