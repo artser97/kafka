3369853ac52fc82398b7853e0c28bcf1ae4fbc3b
artser97 <artser97@yandex.ru>
2023-02-01 18:44:28 +0200

    kafka app init commit

---
+++ src/main/java/com/artser/kafka/sevice/KafkaConsumerService.java
@@ -0,0 +1,20 @@
+package com.artser.kafka.sevice;
+
+import com.artser.kafka.config.AppConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumerService {
+  private final Logger logger =
+      LoggerFactory.getLogger(KafkaConsumerService.class);
+
+  @KafkaListener(topics = AppConstants.TOPIC_NAME,
+      groupId = AppConstants.GROUP_ID)
+  public void consume(String message)
+  {
+    logger.info(String.format("Message received -> %s", message));
+  }
+}
